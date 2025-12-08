741a071614c5bc1a39397c6ccce0cb51316211f4
Simon Luo <simon1uo@163.com>
2025-12-08 14:06:16 +0800

    feat: add font styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@fontsource-variable/inter/index.css'
+import '@fontsource-variable/jetbrains-mono/index.css'
+import '@fontsource/noto-sans-sc/chinese-simplified-400.css'
+import '@fontsource/noto-sans-sc/chinese-simplified-700.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
